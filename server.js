8aad861c60b05d56b67cf2a9cee3dc9ac62ba474
Ahmed Abdelglel <pokelokeyoke1g@gmail.com>
2024-11-12 20:49:27 +0200

    init

--- server.js
+++ server.js
@@ -2,17 +2,25 @@ const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 dotenv.config({ path: "config.env" });
+const dbConnection = require("./config/database");
+const categoryRoute = require("./Routes/categoryRoute");
 
+// Connect to DB
+dbConnection();
+
+// express app
 const app = express();
+
+// Middlewares
+app.use(express.json());
 
 if (process.env.NODE_ENV == "development") {
   app.use(morgan("dev"));
   console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
-app.get("/", (req, res) => {
-  res.send("Our API");
-});
+// Routes
+app.use("/api/v1/categories", categoryRoute);
 
 const PORT = process.env.PORT || 8000;
 
